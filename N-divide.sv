e2c2db4b5a3e58cdd90df1e09ffed61f2ce92f4b
Bob C <166335643+chen-eecgutorontoca@users.noreply.github.com>
2025-04-11 12:47:20 -0700

    Update N-divide.sv

--- N-divide.sv
+++ N-divide.sv
@@ -1,29 +1,36 @@
 module N_divide(
-	input logic clk_out,		//100MHz output clock
-	input logic rst_n,
-	output logic clk_fb			//10M feedback
+  input logic clk,     
+  input logic rst_n,    
+  output logic clk_fb   
 );
-	
-	logic [2:0] cnt;
-	
-	always_ff@(posedge clk_out or negedge rst_n)begin
-		if(!rst_n)begin
-			cnt <= 3'b0;
-		end else if(cnt == 3'b100)begin
-			cnt <= 3'b0;
-		end else begin
-			cnt <= cnt + 1;
-		end
-	end
-	
-	always_ff@(posedge clk_out or negedge rst_n)begin
-		if(!rst_n)begin
-			clk_fb <= 1'b0;
-		end else if(cnt == 3'b100)begin
-			clk_fb <= ~clk_fb;
-		end else begin
-			clk_fb <= clk_fb;
-		end
-	end
-	
+
+  logic [1:0] cnt_pos, cnt_neg; 
+  logic clk_pos, clk_neg;
+
+  always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      cnt_pos <= 2'b00;
+    end else if(cnt_pos == 2'b10) begin
+      cnt_pos <= 2'b00;
+     end else begin 
+       cnt_pos <= cnt_pos + 1;
+    end
+  end
+
+  assign clk_pos = (cnt_pos == 2'b00);
+
+  always_ff @(negedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      cnt_neg <= 2'b00;
+    end else if (cnt_neg == 2'b10)begin
+      cnt_neg <= 2'b00; 
+    end else begin
+      cnt_neg <= cnt_neg + 1;
+    end
+  end
+
+  assign clk_neg = (cnt_neg == 2'b00);
+
+  assign clk_fb = clk_pos | clk_neg;
+
 endmodule
