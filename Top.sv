69374be94ed6f0d2ed4f95375ccca20d59a28105
Snehal Sobti <snehal.sobti@gmail.com>
2025-04-10 23:11:44 -0400

    feat: first draft of specifying io according to tinytapeout

--- Top.sv
+++ Top.sv
@@ -1,3 +1,28 @@
+`default_nettype none
+
+module tt_um_DPLL (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out[7:2]  = 5'b0; 
+  assign uio_out = 7'b0;
+  assign uio_oe  = 7'b0;
+
+  DPLL_top dpll(.clk_ref(clk), .rst_n(rst_n), .pll_out(uo_out[0]), .locked(uo_out[1]));
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, ui_in, uio_in, 1'b0};
+
+endmodule
+
 module DPLL_top(
 	input logic clk_ref,       // Reference clock (10MHz)
 	input logic rst_n,         // Active-low reset
