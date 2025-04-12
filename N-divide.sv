80d3e1967b7049ee00d45eecbf031c7d1ec830a6
Snehal Sobti <106326726+snehalsobti@users.noreply.github.com>
2025-04-12 18:12:47 -0400

    Update N-divide.sv - clk to clk_out and some minor changes

--- N-divide.sv
+++ N-divide.sv
@@ -1,5 +1,5 @@
 module N_divide(
-  input logic clk,    //30MHz  
+  input logic clk_out,    //30MHz  
   input logic rst_n,    
   output logic clk_fb   //10MHz feedback clock with 50% duty cycle
 );
@@ -7,7 +7,7 @@ module N_divide(
   logic [1:0] cnt_pos, cnt_neg; 
   logic clk_pos, clk_neg;
 
-  always_ff @(posedge clk or negedge rst_n) begin
+  always_ff @(posedge clk_out or negedge rst_n) begin
     if (!rst_n) begin
       cnt_pos <= 2'b00;
     end else if(cnt_pos == 2'b10) begin
@@ -17,9 +17,7 @@ module N_divide(
     end
   end
 
-  assign clk_pos = (cnt_pos == 2'b00);
-
-  always_ff @(negedge clk or negedge rst_n) begin
+  always_ff @(negedge clk_out or negedge rst_n) begin
     if (!rst_n) begin
       cnt_neg <= 2'b00;
     end else if (cnt_neg == 2'b10)begin
@@ -29,8 +27,6 @@ module N_divide(
     end
   end
 
-  assign clk_neg = (cnt_neg == 2'b00);
-
-  assign clk_fb = clk_pos | clk_neg;
+  assign clk_fb = cnt_pos == 2'b00 | cnt_neg == 2'b00;
 
 endmodule
