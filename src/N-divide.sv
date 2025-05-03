2949e1e831bbed9a1362190b870fddb35a4a92fb
Bob <bob.y.chan.zj@gmail.com>
2025-05-03 12:17:17 -0700

    Added ATPG function to N-divide

--- src/N-divide.sv
+++ src/N-divide.sv
@@ -1,15 +1,23 @@
 module N_divide(
   input logic clk_out,    //30MHz  
   input logic rst_n,    
-  output logic clk_fb   //10MHz feedback clock with 50% duty cycle
+  output logic clk_fb,   //10MHz feedback clock with 50% duty cycle
+  input logic scan_en,
+  input logic scan_in,
+  output logic scan_out
 );
 
   logic [1:0] cnt_pos, cnt_neg; 
   logic clk_pos, clk_neg;
+  
+  logic scan_chain_connection;				//continuous scan chain
 
   always_ff @(posedge clk_out or negedge rst_n) begin
     if (!rst_n) begin
       cnt_pos <= 2'b00;
+	end else if(scan_en)begin
+	  cnt_pos[0] <= scan_in;
+	  cnt_pos[1] <= cnt_pos[0];	
     end else if(cnt_pos == 2'b10) begin
       cnt_pos <= 2'b00;
      end else begin 
@@ -20,7 +28,10 @@ module N_divide(
   always_ff @(negedge clk_out or negedge rst_n) begin
     if (!rst_n) begin
       cnt_neg <= 2'b00;
-    end else if (cnt_neg == 2'b10)begin
+	end else if(scan_en)begin
+	   cnt_neg[0] <= scan_chain_connection;
+	   cnt_neg[1] <= cnt_neg[0];
+	end else if (cnt_neg == 2'b10)begin
       cnt_neg <= 2'b00; 
     end else begin
       cnt_neg <= cnt_neg + 1;
@@ -28,5 +39,8 @@ module N_divide(
   end
 
   assign clk_fb = cnt_pos == 2'b00 | cnt_neg == 2'b00;
-
+	
+  assign scan_chain_connection = cnt_pos[1];			
+  assign scan_out = cnt_neg[1];
+ 
 endmodule
