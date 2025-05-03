9c157c1397c78e8efdd695431505449155298199
Bob <bob.y.chan.zj@gmail.com>
2025-05-03 12:29:01 -0700

    Add ATPG function to LPF

--- src/Low-Pass_Filter.sv
+++ src/Low-Pass_Filter.sv
@@ -3,7 +3,10 @@ module LPF(  					//low pass filter
 	input logic rst_n,
 	input logic up,
 	input logic down,
-	output logic signed [15:0] filtered_control_signal
+	output logic signed [15:0] filtered_control_signal,
+	input logic scan_in,
+	input logic scan_en,
+	output logic scan_out
 );
 
 	//use PI controller for first order filter transfer function
@@ -13,14 +16,16 @@ module LPF(  					//low pass filter
 	logic signed [15:0] error;
 	logic signed [31:0] phase_accumulator;
 	
+	integer i;
+	
 	//phase error decode
 	always_comb begin
 		if(!rst_n)
 			error = 16'd0;
 		else
 			case({up, down})
-				2'b10: error = 16'd1;
-				2'b01: error = -16'd1;
+				2'b10: error = -16'd1;
+				2'b01: error = 16'd1;
 				2'b00: error = 16'd0;
 				default: error = 16'd0;
 			endcase
@@ -30,6 +35,11 @@ module LPF(  					//low pass filter
 	always_ff@(posedge clk or negedge rst_n)begin				
 		if(!rst_n)begin
 			phase_accumulator <= 32'd0;
+		end else if(scan_en)begin							//scan chain operation when scan_en is asserted
+			phase_accumulator[0] <= scan_in;
+			for(i = 1; i < 32; i++)begin
+				phase_accumulator[i] <= phase_accumulator[i-1];
+			end
 		end else begin
 			phase_accumulator <= phase_accumulator + Ki * error;
 		end
@@ -38,4 +48,7 @@ module LPF(  					//low pass filter
 	//Compute Kp+Ki/s
 	assign filtered_control_signal = phase_accumulator + Kp * error;
 	
+	assign scan_out = phase_accumulator[31];
 endmodule
+
+
